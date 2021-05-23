baf8ef911e02f10e11df90fcbb90e545829df8d8
g119446316 <40300573+g119446316@users.noreply.github.com>
2021-05-23 06:38:17 -0700

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ import hashlib
 
 
 app = Flask(__name__,static_url_path="/templates", static_folder='/home/g/tmp/templates')
-redis = Redis(host='0.0.0.0',port=6379)
+redis = Redis(host='redis',port=6379)
 
 app.config['SECRET_KEY'] = 'secret!'
 #app.secret_key = config.get('flask', 'secret_key')
